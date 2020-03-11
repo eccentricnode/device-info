ac7bcba267a90359ca939c7f103a302aad508232
eccentricnode <shival.bigman@gmail.com>
2020-03-11 16:49:30 -0700

    incremental checkin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+
+const { Device } = Plugins;
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  deviceInfo;
 
   constructor() {}
 
+  async displayDeviceInfo() {
+    this.deviceInfo = await Device.getInfo();
+  }
 }
